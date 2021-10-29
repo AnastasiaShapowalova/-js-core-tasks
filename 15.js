6ad8edba14ed8477eb904558822b8b1efa3f0e30
Anasteisha Shamrelyuk <anasteisha@Anasteishas-MacBook-Air.local>
2021-10-30 00:13:07 +0300

    done task 15

--- 15.js
+++ 15.js
@@ -1,17 +1,8 @@
 function copyOf(item){
-    if (Object.prototype.toString.call(item) === '[object Array]') {
-        let arrayCopy = item.slice(0)
-        return arrayCopy
-    } else if (typeof(item) == 'object') {
-        let objCopy = {}
-        for (key in item) {
-            objCopy[key] = item[key]; // копирует каждое свойство objCopy
-          }
-          return objCopy;
-    } else if (Object.prototype.toString.call(item) === '[object Date]') {
-        var currentDate = new Date();
-        return currentDate;
-    }
+    if (item.constructor === Date) { 
+        return new Date(item)
+    } 
+    return Object.assign( {} ,item);
 }
 
 var a = [1, 2, 3];
@@ -24,8 +15,8 @@ var b = copyOf(a); // b — это отдельный объект
 b.x = 10;
 console.log(a.x); // 1
 
-var c = new Date(2014, 1, 1);
+var c = new Date();
 var d = copyOf(c);
 console.log(d)
-// d.setFullYear(2015);
-// console.log(c.getFullYear())
+d.setFullYear(2015);
+console.log(c.getFullYear())
